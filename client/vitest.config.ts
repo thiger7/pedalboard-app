bd2cedd7fa4504b8fb0383bd69201b0b786536c7
thiger7 <adf0423@gmail.com>
2026-01-28 20:29:15 +0900

    chore: Replace jsdom with happy-dom in package dependencies and update related configurations

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     passWithNoTests: true,
   },
   resolve: {
